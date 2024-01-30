b68679c09bb9d3207eddb0c0c890196cd86f749d
CodeWithAnuruddh <154063415+CodeWithAnuruddh@users.noreply.github.com>
2024-01-30 09:54:31 +0530

    ggg

--- project 4/script.js
+++ project 4/script.js
@@ -8,14 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
     buttonDecrease.addEventListener('click', function(e) {
         e.preventDefault();
         timer.textContent = valueDecrease();
+        color();
+       
         
-
     });
 
     buttonReset.addEventListener('click', function(e) {
         e.preventDefault();
         count = 0;
         timer.textContent = count;
+        color();
         
         
     });
@@ -23,9 +25,24 @@ document.addEventListener("DOMContentLoaded", () => {
     buttonIncrease.addEventListener('click', function(e) {
         e.preventDefault();
         timer.textContent = ++count;
+        color();
+       
+        
     });
 
     function valueDecrease() { 
        return( --count); 
     }
+    function color(){
+        if(count>0){
+            timer.style.color = 'red';
+        }
+        else if(count<0){
+            timer.style.color = 'aqua';
+        }
+        else{
+            timer.style.color = '#102A42'
+        }
+    }
 });
+  
